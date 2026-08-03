a352d312e5750735acbd21d61304368be2f1bcf0
Chris Walter <chrishwalter00@gmail.com>
2026-08-03 10:11:54 -0400

    Created initial files

---
+++ CreateMaze.py
@@ -0,0 +1,8 @@
+
+def genMaze(height, width):
+    pass
+
+if __name__ == "__main__":
+    height = int(input("Enter the height of the maze: "))
+    width = int(input("Enter the width of the maze: "))
+    maze = genMaze(height, width)
---
+++ MazeSolver.py
@@ -0,0 +1,5 @@
+def solveMaze(maze):
+    pass
+
+if __name__ == "__main__":
+    filename = input("Enter the filename of the maze: ")
